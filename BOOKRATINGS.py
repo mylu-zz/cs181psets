da39d7c963891ff2dcb585bc19f27ebc88914965
Olivia A <liv810@gmail.com>
2014-02-09 14:06:36 -0500

    in process of writing lil matrix

--- BOOKRATINGS.py
+++ BOOKRATINGS.py
@@ -18,17 +18,23 @@ n_feats = 50 #number of features to train
 n_users = users.shape[0] #number of users
 n_books = books.shape[0] #number of books
 
+# Create mapping for books to indices of sparse matrix
+book_mappings = dict(zip(list(books['ISBN'].unique()), range(n_books)))
+# Add book indices to 'train' DataFrame for later lookup
+train['Book_Index'] = [book_mappings[x] for x in train['ISBN']]
+
 # create book feature matrix
 book_features = pd.DataFrame(index = list(books['ISBN'].unique()), columns = range(n_feats))
 # create user feature matrix
 user_features = pd.DataFrame(index = ['None']+list(users['User'].unique()), columns = range(n_feats))
 
-# Create mapping for books to indices of sparse matrix
-book_mappings = dict(zip(list(books['ISBN'].unique()), range(n_books)))
-
 # create matrix with users and movie ratings
-ratings = sp.sparse.csr_matrix((n_books, n_users+1))
-zip(train[book_mappings['ISBN']],train['User']
+ratings = sp.sparse.lil_matrix((n_books, n_users+1))
+# populate ratings matrix with the book:user ratings 309
+for i in range(200):
+  ratings[train['Book_Index'][1000*i:1000*i+999], train['User'][1000*i:1000*i+999]] = train['Rating'][1000*i:1000*i+999]
+  print i
+#zip(train[book_mappings['ISBN']],train['User']
 
 
 
