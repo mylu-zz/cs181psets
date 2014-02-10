7e1511693d15e16bace85703e60ba91f67afde53
Olivia A <liv810@gmail.com>
2014-02-10 01:56:37 -0500

    Debugged

--- BOOKRATINGS.py
+++ BOOKRATINGS.py
@@ -14,7 +14,7 @@ print train.head()
 print users.head()
 print books.head()
 
-n_feats = 50 #number of features to train
+n_feats = 10 #number of features to train
 n_users = users.shape[0] #number of users
 n_books = len(list(books['ISBN'].unique())) #number of unique books
 
@@ -24,9 +24,9 @@ book_mappings = dict(zip(list(books['ISBN'].unique()), range(n_books)))
 train['Book_Index'] = [book_mappings[x] for x in train['ISBN']]
 
 # create book feature matrix n_users by n_feats
-book_features = pd.DataFrame(np.random.randn(n_users, n_feats), index = list(books['ISBN'].unique()), columns = range(n_feats))
+book_features = pd.DataFrame(np.random.randn(n_feats, n_books), index = range(n_feats), columns = list(books['ISBN'].unique()))
 # create user feature matrix n_feats by n_books
-user_features = pd.DataFrame(np.random.randn(n_feats, n_books), index = ['None']+list(users['User'].unique()), columns = range(n_books))
+user_features = pd.DataFrame(np.random.randn(n_users + 1, n_feats), index = ['None']+list(users['User'].unique()), columns = range(n_feats))
 
 """
 # Run this section of code to initialize the ratings matrix with training data
@@ -34,8 +34,8 @@ user_features = pd.DataFrame(np.random.randn(n_feats, n_books), index = ['None']
 ratings = sp.sparse.lil_matrix((n_books, n_users+1))
 # populate ratings matrix with the book:user ratings
 for i in range(200):
-  ratings[train['Book_Index'][1000*i:1000*i+999], train['User'][1000*i:1000*i+999]] = train['Rating'][1000*i:1000*i+999]
-  print i
+	ratings[train['Book_Index'][1000*i:1000*i+999], train['User'][1000*i:1000*i+999]] = train['Rating'][1000*i:1000*i+999]
+	print i
 ratings[train['Book_Index'][199999], train['User'][199999]] = train['Rating'][199999]
 
 np.save("ratings_matrix.out.npy", ratings)
@@ -52,22 +52,24 @@ with open('ratings_matrix.dat', 'rb') as infile:
 """
 
 THRESHOLD = 0.0001
+alpha = 0.0001
+beta = 0.0002
 
 # make list of tuples with book index, user index, and rating
-rating_datapoints = zip(train['ISBN'],train['User'], train['Rating'])
+rating_datapoints = zip(train['Book_Index'],train['User'],train['Rating'])
 converged = False
 iteration = 0
 while not converged:
 	print iteration
 	iteration += 1
 	for dp in rating_datapoint:
-		error = dp[2] - np.dot(user_features[dp[0],:],book_features[;,dp[1]) + beta/2 * (np.sum(np.square(user_features)) + np.sum(np.square(user_features)))
+		error = dp[2] - np.dot(user_features.irow(dp[1]),book_features.icol(dp[0])) + beta/2 * (np.sum(np.square(user_features.as_matrix())) + np.sum(np.square(book_features.as_matrix())))
 		for feature in range(n_feats):
-			user_features[dp[0]][feature] += alpha * (2 * error * book_features[feature][dp[1]] - beta * user_features[dp[0]][feature])
-			book_features[feature][dp[1]] += alpha * (2 * error * user_features[dp[0]][feature] - beta * book_features[feature][dp[1]])
+			user_features.iloc[dp[1],feature] += alpha * (2 * error * book_features.iloc[feature,dp[0]] - beta * user_features.iloc[dp[1],feature])
+			book_features.iloc[feature,dp[0]] += alpha * (2 * error * user_features.iloc[dp[1],feature] - beta * book_features.iloc[feature,dp[0]])
 	cumulative_error = 0
 	for dp in rating_datapoint:
-		cumulative_error += pow(dp[2] - np.dot(user_features[dp[0],:],book_features[;,dp[1]),2) + beta/2 * (np.sum(np.square(user_features[dp[0],:]])) + np.sum(np.square(book_features[:,dp[1],:]])))
+		cumulative_error += pow(dp[2] - np.dot(user_features.irow(dp[1]),book_features.icol(dp[0])),2) + beta/2 * (np.sum(np.square(user_features.irow(dp[1]))) + np.sum(np.square(book_features.icol(dp[0]))))
 	if cumulative_error < THRESHOLD:
 		converged = True
 
